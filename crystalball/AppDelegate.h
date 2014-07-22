ba1e0201617d985a4caa2e2514613d72b7ec681d
sqx <ugo.mare.epitech@gmail.com>
2014-07-22 03:37:14 +0200

    Initial Commit

---
+++ crystalball/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  crystalball
+//
+//  Created by Ugo on 22/07/14.
+//  Copyright (c) 2014 Ugo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
